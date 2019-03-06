77a691994cc14ad9436fdad6bedcd9d779791146
Sakthivel Raja <39544685+sakthivelraja@users.noreply.github.com>
2019-03-05 21:57:19 -0600

    Add files via upload

---
+++ src/app/Welcome/welcome.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'welcome-quiz',
+  templateUrl: './welcome.component.html',
+  styleUrls: [ './welcome.component.css' ]
+})
+export class WelcomeComponent  {
+   title = 'Welcome to Quiz App';
+}
