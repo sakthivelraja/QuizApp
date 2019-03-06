81d92e4a2d19ea6a0cca8986d0aa77e8c6f2e3a2
Sakthivel Raja <39544685+sakthivelraja@users.noreply.github.com>
2019-03-05 21:55:13 -0600

    Add files via upload

---
+++ src/app/Models/QuizStructure.ts
@@ -0,0 +1,9 @@
+export class QuizStructure {
+  QuestionNumber: number;
+  Question: string;
+  OptionOne: string;
+  OptionTwo: string;
+  OptionThree: string;
+  OptionFour: string;
+  CorrectAnswer: string;
+}
